25caad818e81acf8e7cab47d6274fee83c4c105e
Gordon Sun <sunshineo@gmail.com>
2019-04-13 17:31:55 -0700

    Created a bot with schedule.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: probot run index.js
---
+++ index.js
@@ -0,0 +1,37 @@
+const createScheduler = require('probot-scheduler')
+
+
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Application} app
+ */
+module.exports = app => {
+
+  // Your code here
+  app.log('Yay, the app was loaded!')
+
+  createScheduler(app, {
+    interval: 60 * 1000 // 1 day
+  })
+  app.on('schedule.repository', context => {
+    // this event is triggered on an interval, which is 1 hr by default
+    console.log('schedule.repository was triggered')
+    const params = context.repo()
+    console.log('repo name: ' + params.repo)
+  })
+
+  app.on('issues.opened', async context => {
+    console.log('a new issue was opened')
+    const params = context.repo()
+    console.log('repo name: ' + params.repo)
+
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    return context.github.issues.createComment(issueComment)
+  })
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
